799406b85ad9c3cd4116ed37bca88ddd69fd5bc1
Yoann Couble <yoann.couble@valraiso.fr>
2024-06-25 11:04:39 +0200

    restored test + set openai as optional peer dependency

---
+++ src/test.ts
@@ -0,0 +1,30 @@
+import type { Impacts } from "./index";
+import { Ecologits } from "./index";
+Ecologits.init();
+import OpenAI from "openai";
+
+import type OpenAITypes from "openai"; // TODO : remove dependency
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const main = async () => {
+  try {
+    const response = (await client.chat.completions.create({
+      messages: [{ role: "user", content: "Tell me a funny joke!" }],
+      model: "gpt-3.5-turbo",
+    })) as OpenAITypes.Chat.Completions.ChatCompletion & { impacts: Impacts };
+    // Get estimated environmental impacts of the inference
+    console.log(`Response: ${JSON.stringify(response, undefined, 2)}`);
+    console.log(
+      `Energy consumption: ${response.impacts.energy.value} ${response.impacts.energy.unit}`
+    );
+    console.log(
+      `GHG emissions: ${response.impacts.gwp.value} ${response.impacts.gwp.unit}`
+    );
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+main();
