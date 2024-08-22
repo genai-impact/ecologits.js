f2a3afa6e87c32fc1d582139002d751cb70b3319
Yoann Couble <yoann.couble@valraiso.fr>
2024-08-22 09:41:55 +0200

    model not found --> undefined impacts

--- packages/ecologits.js/src/impacts/default.ts
+++ packages/ecologits.js/src/impacts/default.ts
@@ -4,25 +4,25 @@ const energy: ImpactMetric = {
   type: "energy",
   name: "Energy",
   unit: "kWh",
-  value: 0,
+  value: undefined,
 };
 const gwp: ImpactMetric = {
   type: "GWP",
   name: "Global Warming Potential",
   unit: "kgCO2eq",
-  value: 0,
+  value: undefined,
 };
 const adpe: ImpactMetric = {
   type: "ADPe",
   name: "Abiotic Depletion Potential (elements)",
   unit: "kgSbeq",
-  value: 0,
+  value: undefined,
 };
 const pe: ImpactMetric = {
   type: "PE",
   name: "Primary Energy",
   unit: "MJ",
-  value: 0,
+  value: undefined,
 };
 
 export const DEFAULT_IMPACT: Impacts = {
